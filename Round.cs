95e5675fe88ca9e322847e2acd3209e4e4daeed5
Zach Moore <zach@intellitechture.com>
2012-09-26 21:22:08 -0700

    Added images as resource files only. Added basic framework classes: Persona, Player, Round.

---
+++ Round.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialExchange
+{
+    public abstract class Round
+    {
+        public Player Player { get; protected set; }
+        public DateTime BeginRoundTimestamp { get; protected set; }
+        public DateTime EndRoundTimestamp { get; protected set; }
+
+        public Round(Player player)
+        {
+            Player = player;
+        }
+
+        public DateTime BeginRound()
+        {
+            return BeginRoundTimestamp = DateTime.Now;
+        }
+
+        public DateTime EndRound()
+        {
+            return EndRoundTimestamp = DateTime.Now;
+        }
+    }
+}
